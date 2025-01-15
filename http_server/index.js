fce7257268318d3e65a02265ee454e0275442820
tusquake <tusharrealme@gmail.com>
2025-01-15 20:50:12 +0530

    vid_9_http_methods

--- http_server/index.js
+++ http_server/index.js
@@ -1,25 +1,42 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
     console.log('Request Received');
     console.log(req.headers);
     const log = `\n${Date.now()} ${req.url}\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile('logg.txt', log, (err) => {
-        switch(req.url){
+        if (err) throw err;
+        
+        switch(myUrl.pathname) {
             case '/':
                 res.end('Hello From Server');
                 break;
             case '/about':
-                res.end('Welcome to About Page');
+                const username = myUrl.query.myname;
+                res.end(`Welcome to About Page ${username}`);
                 break;
             case '/contact':
                 res.end('Welcome to Contact Page');
                 break;
-            default :
-            res.end('404 Page Not Found');
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end(`You are searching for ${search}`);
+                break;
+            case '/signup':
+                if(req.method==="GET") {
+                    res.end('Welcome to Signup Page');
+                }
+                else if(req.method==="POST") { 
+                    res.end('Successfully Signed Up');
+                }
+            default:
+                res.end('404 Page Not Found');
         }
     });
 });
 
-myServer.listen(3000, () => console.log('Server is running on port 3000'));  
+myServer.listen(3000, () => console.log('Server is running on port 3000'));
